0bc8dd131b0868b71119433b22f89bde979f5131
joeyb <joeybesseling@icloud.com>
2023-04-12 22:31:04 +0200

    added agg config and started with feature engineering

--- assignment1/utils/feature_engineering.py
+++ assignment1/utils/feature_engineering.py
@@ -8,13 +8,49 @@ def feature_engineering():
         values='value', 
         index=['id', 'date'],
         columns=['variable'], 
-        aggfunc=[np.sum, np.mean]).reset_index()
-
+        aggfunc=[np.sum, np.mean, "count"]).reset_index()
+    df = remove_days_with_no_mood(df)
+    df = compute_features(df)
     store_features_and_targets(df)
     
-def find_consecutive_moods(df):
+def remove_days_with_no_mood(df):
+    return df[df.mood.count > 0]
+    
+def find_consecutive_moods(df, window = 5):
     start_end_indices = []
     return start_end_indices
+
+def compute_features(df, window = 5):
+    features_to_keep = df.columns
+    # indices = find_consecutive_moods()
+    targets = df["mood"]
+    features = df.rolling(window).apply(agg_config)
     
 def store_features_and_targets(df: pd.DataFrame, filename: str = "data_features_targets", path: str = "../data/"):
     df.to_csv(path + filename + ".csv")
+    
+agg_config = {
+    "appCat.builtin": [],
+    "appCat.communication": [],
+    "appCat.entertainment": [],
+    "appCat.finance": [],
+    "appCat.game": [],
+    "appCat.office": [],
+    "appCat.other": [],
+    "appCat.social": [],
+    "appCat.travel": [],
+    "appCat.unknown": [],
+    "appCat.utilities": [],
+    "appCat.weather": [],
+    "appCat.": [],
+    "appCat.": [],
+    "appCat.": [],
+    "appCat.": [],
+    "screen": [],
+    "call": [],
+    "sms": [],
+    "activity": [],
+    "circumplex.arousal": [],
+    "circumplex.valence": [],
+    "mood": [],
+}
--- assignment1/utils/main.py
+++ assignment1/utils/main.py
@@ -2,7 +2,6 @@ from data_cleaning import clean_data
 from feature_engineering import feature_engineering
 from model_estimation import model_functions
 
-
 if __name__ == "__main__":
     clean_data()
     feature_engineering()
